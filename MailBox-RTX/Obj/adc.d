3af3238a2c2c750f561b931f0ee9f46b7dfa2ef3
fma23 <fma23@sfu.ca>
2016-02-22 01:04:29 -0800

    Deleting files

--- MailBox-RTX/Obj/adc.d
+++
@@ -1,6 +0,0 @@
-.\obj\adc.o: ADC.c
-.\obj\adc.o: C:\Keil_v5\ARM\RV31\INC\RTL.h
-.\obj\adc.o: C:\Keil_v5\ARM\Inc\Philips\LPC23xx.H
-.\obj\adc.o: C:\Keil_v5\ARM\ARMCC\Bin\..\include\math.h
-.\obj\adc.o: C:\Keil_v5\ARM\ARMCC\Bin\..\include\stdio.h
-.\obj\adc.o: C:\Keil_v5\ARM\ARMCC\Bin\..\include\stdlib.h
--- MailBox-RTX/Obj/dac.d
+++
@@ -1,6 +0,0 @@
-.\obj\dac.o: DAC.c
-.\obj\dac.o: C:\Keil_v5\ARM\RV31\INC\RTL.h
-.\obj\dac.o: C:\Keil_v5\ARM\Inc\Philips\LPC23xx.H
-.\obj\dac.o: C:\Keil_v5\ARM\ARMCC\Bin\..\include\math.h
-.\obj\dac.o: C:\Keil_v5\ARM\ARMCC\Bin\..\include\stdio.h
-.\obj\dac.o: C:\Keil_v5\ARM\ARMCC\Bin\..\include\stdlib.h
