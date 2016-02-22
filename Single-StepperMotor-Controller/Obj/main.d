6248bd4e5928b401216285883d0347a2e613bdf9
fma23 <fma23@sfu.ca>
2016-02-22 00:59:11 -0800

    Deleting files

--- Single-StepperMotor-Controller/Obj/main.d
+++
@@ -1,5 +0,0 @@
-.\obj\main.o: main.c
-.\obj\main.o: C:\Keil_v5\ARM\ARMCC\Bin\..\include\stdio.h
-.\obj\main.o: C:\Keil_v5\ARM\Inc\Philips\LPC23xx.H
-.\obj\main.o: motor.h
-.\obj\main.o: C:\Keil_v5\ARM\ARMCC\Bin\..\include\math.h
